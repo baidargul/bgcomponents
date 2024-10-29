5f129b65eb5e1b0f09855d382a49698cf12b2fd2
Baidar Gul <baidargul@outlook.com>
2024-10-29 13:42:32 +0500

    created experimental content component

---
+++ components/Experimental/ExperimentalContent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ExperimentalContent = (props: Props) => {
+  return <div>ExperimentalContent</div>;
+};
+
+export default ExperimentalContent;
