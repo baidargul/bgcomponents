14a14ccd420b4153bc4833658bdbc2ebcd4cfe2d
Baidar Gul <baidargul@outlook.com>
2024-10-29 13:44:42 +0500

    Starting working on cliped image

--- components/Experimental/ExperimentalContent.tsx
+++ components/Experimental/ExperimentalContent.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import ClipedImage from "./_components/ClipedImage";
 
 type Props = {};
 
 const ExperimentalContent = (props: Props) => {
-  return <div>ExperimentalContent</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="opacity-60 tracking-wider py-1 border-y ">
+        Experimental Content
+      </div>
+      <div>
+        <ClipedImage />
+      </div>
+    </div>
+  );
 };
 
 export default ExperimentalContent;
---
+++ components/Experimental/_components/ClipedImage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ClipedImage = (props: Props) => {
+  return <div>ClipedImage</div>;
+};
+
+export default ClipedImage;
