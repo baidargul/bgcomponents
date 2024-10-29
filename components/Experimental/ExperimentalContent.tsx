fc78584c5c56e9c45bcf595f8d3e08b4c53c4098
Baidar Gul <baidargul@outlook.com>
2024-10-29 13:45:22 +0500

    experiment content label capture no events from now on

--- components/Experimental/ExperimentalContent.tsx
+++ components/Experimental/ExperimentalContent.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const ExperimentalContent = (props: Props) => {
   return (
     <div className="flex flex-col gap-2">
-      <div className="opacity-60 tracking-wider py-1 border-y ">
+      <div className="opacity-60 tracking-wider py-1 border-y pointer-events-none select-none">
         Experimental Content
       </div>
       <div>
