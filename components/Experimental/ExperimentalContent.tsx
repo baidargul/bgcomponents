ae44be29426be49378ae92292a721e27908b03cb
Baidar Gul <baidargul@outlook.com>
2024-10-29 21:30:01 +0500

    Expermental UI

--- components/Experimental/ExperimentalContent.tsx
+++ components/Experimental/ExperimentalContent.tsx
@@ -6,8 +6,8 @@ type Props = {};
 const ExperimentalContent = (props: Props) => {
   return (
     <div className="flex flex-col gap-2">
-      <div className="opacity-60 tracking-wider py-1 border-y pointer-events-none select-none text-center mb-2">
-        Experimental Content
+      <div className="opacity-60 my-4 border-amber-200 bg-amber-50 text-amber-800 tracking-wider py-1 border-y pointer-events-none select-none text-center mb-2">
+        ⚠️ Experimental Content
       </div>
       <div></div>
     </div>
