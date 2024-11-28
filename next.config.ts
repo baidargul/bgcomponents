53f19506131858bc7d47b9091861b92751614fb1
Baidar Gul <baidargul@outlook.com>
2024-11-28 20:54:22 +0500

    addded image check

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["portfolio-swart-eight-52.vercel.app"], // Bas domain name dena hai
+  },
 };
 
 export default nextConfig;
